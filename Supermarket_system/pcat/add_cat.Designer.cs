4a24763b4d8873843bdcddb8e18068d475f75361
MohammedAzaiza <moon_love665@hotmail.com>
2012-06-15 18:40:30 +0300

    pcat_manegment

---
+++ Supermarket_system/pcat/add_cat.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace Supermarket_system.pcat
+{
+    partial class add_cat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.formFrameSkinner1 = new Elegant.Ui.FormFrameSkinner();
+            this.SuspendLayout();
+            // 
+            // formFrameSkinner1
+            // 
+            this.formFrameSkinner1.Form = this;
+            // 
+            // add_cat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 262);
+            this.Name = "add_cat";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.RightToLeftLayout = true;
+            this.Text = "اضافة صنف";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Elegant.Ui.FormFrameSkinner formFrameSkinner1;
+    }
+}
