0d07332ecf989cdb3d0cd8e8c91fa2d1c7622381
abdullah abunada <eng.a.abunada@gmail.com>
2012-06-19 19:51:56 +0300

    debit

---
+++ Supermarket_system/debit/customer_debit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Supermarket_system.debit
+{
+    public partial class customer_debit : Form
+    {
+        public customer_debit()
+        {
+            InitializeComponent();
+        }
+    }
+}
